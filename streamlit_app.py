5801825394450d8dd75be93bd2e414a69658c92c
chirayu Sharma <111888038+absolution-end@users.noreply.github.com>
2024-08-25 01:05:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+import pandas as pd
 st.title('Belto-B')
 # st.title('')
 st.write('Product work on image/video analysis model')
+df =pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+df
