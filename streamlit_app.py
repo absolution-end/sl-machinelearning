f2e43a6f542ed6bdd7c16a837036db1660cd6296
chirayu Sharma <111888038+absolution-end@users.noreply.github.com>
2024-08-25 01:10:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ st.title('Belto-B')
 # st.title('')
 st.write('Product work on image/video analysis model')
 with st.expander('Data'):
-  st.write('**Raw Data**'
+  st.write('**Raw Data**')
   df =pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
-df
+  df
